c6af949fe9eb3a7aee3076c685268c79a5529f36
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2022-11-21 08:49:13 +0900

    ドラゴンのモーション追加

--- stage/enemy/Enemy.cpp
+++ stage/enemy/Enemy.cpp
@@ -31,15 +31,8 @@ void Enemy::Update()
 void Enemy::Draw(ViewProjection viewProjection)
 {
 	//modelDoragon[0]->Draw(worldTransform_[0], viewProjection);//ëÂå≥
-	//modelDoragon[1]->Draw(worldTransform_[1], viewProjection);//ì™
-	//modelDoragon[2]->Draw(worldTransform_[2], viewProjection);//ä{
-	////éÒ
-	//modelDoragon[3]->Draw(worldTransform_[3], viewProjection);
-	//modelDoragon[4]->Draw(worldTransform_[4], viewProjection);
-	//modelDoragon[5]->Draw(worldTransform_[5], viewProjection);
-	//modelDoragon[6]->Draw(worldTransform_[6], viewProjection);
-	//modelDoragon[7]->Draw(worldTransform_[7], viewProjection);
 
+	// äeÉpÅ[Éc(ì™ÅAä{ÅAéÒ)
 	for (int i = 1; i < modelNum; i++)
 	{
 		modelDoragon[i]->Draw(worldTransform_[i], viewProjection);
@@ -51,68 +44,69 @@ void Enemy::ParentInitialize()
 	worldTransform_[0].Initialize();
 
 	//ì™
-	worldTransform_[1].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[1].translation_ = { 0.0f,14.0f,-4.0f }; 
+	//worldTransform_[1].scale_ = Vector3(20.0f, 20.0f, 20.0f);
+	//worldTransform_[1].translation_ = { 0.0f,14.0f,-4.0f }; 
+	worldTransform_[1].translation_ = { 0.0f,ParPos(14.0f),ParPos(-4.0f) };
 	worldTransform_[1].parent_ = &worldTransform_[0];
 	worldTransform_[1].Initialize();
 
 	//ä{
-	worldTransform_[2].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[2].translation_ = { 0.0f,14.0f,-4.0f };
+	//worldTransform_[2].scale_ = Vector3(20.0f, 20.0f, 20.0f);
+	worldTransform_[2].translation_ = { 0.0f,ParPos(14.0f),ParPos(-4.0f) };
 	worldTransform_[2].parent_ = &worldTransform_[0];
 	worldTransform_[2].Initialize();
 
 	//éÒ
-	worldTransform_[3].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[3].translation_ = { 0.0f,15.0f,2.0f };
+	//worldTransform_[3].scale_ = Vector3(20.0f, 20.0f, 20.0f);
+	worldTransform_[3].translation_ = { 0.0f,ParPos(15.0f),ParPos(2.0f) };
 	worldTransform_[3].rotation_.x = -90 * PI / 180;
 	worldTransform_[3].parent_ = &worldTransform_[0];
 	worldTransform_[3].Initialize();
 
-	worldTransform_[4].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[4].translation_ = { 0.0f,14.0f,4.5f }; 
+	//worldTransform_[4].scale_ = Vector3(20.0f, 20.0f, 20.0f);
+	worldTransform_[4].translation_ = { 0.0f,ParPos(14.0f),ParPos(4.5f) };
 	worldTransform_[4].rotation_.x = -50 * PI / 180;
 	worldTransform_[4].parent_ = &worldTransform_[0];
 	worldTransform_[4].Initialize();
 
-	worldTransform_[5].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[5].translation_ = { 0.0f,11.5f,6.0f }; 
+	//worldTransform_[5].scale_ = Vector3(20.0f, 20.0f, 20.0f);
+	worldTransform_[5].translation_ = { 0.0f,ParPos(11.5f),ParPos(6.0f) };
 	worldTransform_[5].rotation_.x = 6 * PI / 180;
 	worldTransform_[5].parent_ = &worldTransform_[0];
 	worldTransform_[5].Initialize();
 
-	worldTransform_[6].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[6].translation_ = { 0.0f,8.0f,5.0f }; 
+	//worldTransform_[6].scale_ = Vector3(20.0f, 20.0f, 20.0f);
+	worldTransform_[6].translation_ = { 0.0f,ParPos(8.0f),ParPos(5.0f) }; 
 	worldTransform_[6].rotation_.x = 43 * PI / 180;
 	worldTransform_[6].parent_ = &worldTransform_[0];
 	worldTransform_[6].Initialize();
 
-	worldTransform_[7].scale_ = Vector3(23.0f, 23.0f, 23.0f);
-	worldTransform_[7].translation_ = { 0.0f,5.5f,4.0f };
+	worldTransform_[7].scale_ = Vector3(1.15f, 1.15f, 1.15f);
+	worldTransform_[7].translation_ = { 0.0f,ParPos(5.5f),ParPos(4.0f) };
 	worldTransform_[7].rotation_.x = 16 * PI / 180;
 	worldTransform_[7].parent_ = &worldTransform_[0];
 	worldTransform_[7].Initialize();
 
-	worldTransform_[8].scale_ = Vector3(26.0f, 26.0f, 26.0f);
-	worldTransform_[8].translation_ = { 0.0f,2.2f,3.5f };
+	worldTransform_[8].scale_ = Vector3(1.3f, 1.3f, 1.3f);
+	worldTransform_[8].translation_ = { 0.0f,ParPos(2.2f),ParPos(3.5f) };
 	worldTransform_[8].rotation_.x = 4 * PI / 180;
 	worldTransform_[8].parent_ = &worldTransform_[0];
 	worldTransform_[8].Initialize();
 
-	worldTransform_[9].scale_ = Vector3(31.0f, 31.0f, 31.0f);
-	worldTransform_[9].translation_ = { 0.0f,-2.0f,3.5f };
+	worldTransform_[9].scale_ = Vector3(1.55f, 1.55f, 1.55f);
+	worldTransform_[9].translation_ = { 0.0f,ParPos(-2.0f),ParPos(3.5f) };
 	worldTransform_[9].rotation_.x = -4 * PI / 180;
 	worldTransform_[9].parent_ = &worldTransform_[0];
 	worldTransform_[9].Initialize();
 
-	worldTransform_[10].scale_ = Vector3(37.0f, 37.0f, 37.0f);
-	worldTransform_[10].translation_ = { 0.0f,-7.0f,4.0f };
+	worldTransform_[10].scale_ = Vector3(1.85f, 1.85f, 1.86f);
+	worldTransform_[10].translation_ = { 0.0f,ParPos(-7.0f),ParPos(4.0f) }; 
 	worldTransform_[10].rotation_.x = -4 * PI / 180;
 	worldTransform_[10].parent_ = &worldTransform_[0];
 	worldTransform_[10].Initialize();
 
-	worldTransform_[11].scale_ = Vector3(42.0f, 42.0f, 42.0f);
-	worldTransform_[11].translation_ = { 0.0f,-12.5f,4.5f }; 
+	worldTransform_[11].scale_ = Vector3(2.1f, 2.1f, 2.1f);
+	worldTransform_[11].translation_ = { 0.0f,ParPos(-12.5f),ParPos(4.5f) };
 	worldTransform_[11].rotation_.x = -5 * PI / 180;
 	worldTransform_[11].parent_ = &worldTransform_[0];
 	worldTransform_[11].Initialize();
@@ -120,62 +114,57 @@ void Enemy::ParentInitialize()
 
 void Enemy::ParentUpdate()
 {
-	ParentSetPosition();
-
+	StandbyMotion();
+	BreathMotion();
 	for (int i = 0; i < modelNum; i++)
 	{
 		worldTransform_[i].Update();
 	}
 }
 
-void Enemy::ParentSetPosition()
+void Enemy::StandbyMotion()
 {
-	Vector3 original = worldTransform_[0].translation_;
+	if(isUp == true)
+	{
+		standbyRot += 0.1f;
+		
+		if(standbyRot >= 0.0f)
+		{
+			isUp = false;
+		}
+	}
+	if(isUp == false)
+	{
+		standbyRot -= 0.1f;
 
-	//ì™
-	worldTransform_[1].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[1].translation_ = Vector3(original.x, original.y + 14.0f, original.z - 4.0f);
+		if (standbyRot <= -10.0f)
+		{
+			isUp = true;
+		}
+	}
 
-	//ä{
-	worldTransform_[2].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[2].translation_ = Vector3(original.x, original.y + 14.0f, original.z - 4.0f);
+	for (int i = 1; i < 3; i++)//ì™ÅAä{
+	{
+		worldTransform_[i].rotation_.x = standbyRot * PI / 180;
+	}
+}
 
-	//éÒ
-	worldTransform_[3].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[3].translation_ = Vector3(original.x, original.y + 15.0f, original.z + 2.0f);
-	worldTransform_[3].rotation_.x = -90 * PI / 180;
+void Enemy::BreathMotion()
+{
+	if(input_->PushKey(DIK_A))
+	{
+		Rot += 1;
+	}
+	if(input_->PushKey(DIK_D))
+	{
+		Rot -= 1;
+	}
+	worldTransform_[0].rotation_.y = Rot * PI / 180;
+}
 
-	worldTransform_[4].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[4].translation_ = Vector3(original.x, original.y + 14.0f, original.z + 4.5f);
-	worldTransform_[4].rotation_.x = -50 * PI / 180;
+void Enemy::BiteMotion()
+{
 
-	worldTransform_[5].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[5].translation_ = Vector3(original.x, original.y + 11.5f, original.z + 6.0f);
-	worldTransform_[5].rotation_.x = 6 * PI / 180;
-
-	worldTransform_[6].scale_ = Vector3(20.0f, 20.0f, 20.0f);
-	worldTransform_[6].translation_ = Vector3(original.x, original.y + 8.0f, original.z + 5.0f);
-	worldTransform_[6].rotation_.x = 43 * PI / 180;
-
-	worldTransform_[7].scale_ = Vector3(23.0f, 23.0f, 23.0f);
-	worldTransform_[7].translation_ = Vector3(original.x, original.y + 5.5f, original.z + 4.0f);
-	worldTransform_[7].rotation_.x = 16 * PI / 180;
-
-	worldTransform_[8].scale_ = Vector3(26.0f, 26.0f, 26.0f);
-	worldTransform_[8].translation_ = Vector3(original.x, original.y + 2.2f, original.z + 3.5f);
-	worldTransform_[8].rotation_.x = 4 * PI / 180;
-
-	worldTransform_[9].scale_ = Vector3(31.0f, 31.0f, 31.0f);
-	worldTransform_[9].translation_ = Vector3(original.x, original.y - 2.0f , original.z + 3.5f);
-	worldTransform_[9].rotation_.x = -4 * PI / 180;
-
-	worldTransform_[10].scale_ = Vector3(37.0f, 37.0f, 37.0f);
-	worldTransform_[10].translation_ = Vector3(original.x, original.y - 7.0f, original.z + 4.0f);
-	worldTransform_[10].rotation_.x = -4 * PI / 180;
-
-	worldTransform_[11].scale_ = Vector3(42.0f, 42.0f, 42.0f);
-	worldTransform_[11].translation_ = Vector3(original.x, original.y - 12.5f, original.z + 4.5f);
-	worldTransform_[11].rotation_.x = -5 * PI / 180;
 }
 
 
--- stage/enemy/Enemy.h
+++ stage/enemy/Enemy.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include "collider/Collider.h"
 #include "WorldTransform.h"
 #include "Model.h"
@@ -21,6 +21,10 @@ private:
 	float y = 0.0f;
 	float z = 0.0f;
 	float s = 0.0f;
+	bool isStandby = true;
+	uint16_t timer = 30;
+	float standbyRot = 0.0f;
+	bool isUp = false;
 	void Move();
 public:
 	void Initialize();
@@ -31,9 +35,16 @@ public:
 	const Vector3 GetWorldPosition() { return worldTransform_[0].translation_; }
 	const Vector3 GetRadius() { return worldTransform_[0].scale_; }
 
-	void ParentInitialize();
+	float ParPos(float x) { return x / 20.0f; }//親子関係にすると座標がおかしくなるため
+
+	void ParentInitialize();	//親子関係の初期化
 
 	void ParentUpdate();
 
-	void ParentSetPosition();
+	void StandbyMotion();		//待機のモーション
+
+	void BreathMotion();		//ブレスのモーション
+
+	void BiteMotion();			//嚙みつきのモーション
+
 };
